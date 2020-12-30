911ecf8cb9aa3c00609bc2e01ead3e0b29bb16c8
ablazleon <ablazleon@gmail.com>
2020-12-30 18:03:56 +0100

    Cannot read property cacheCall of undefined

--- dapp/src/components/Alumnos/Matricular.js
+++ dapp/src/components/Alumnos/Matricular.js
@@ -5,6 +5,18 @@ const Matricular = ({drizzle, drizzleState}) => <MatricularV1  drizzle={drizzle}
 
 const MatricularV1 = ({drizzle, drizzleState}) => <article className="AppMisDatos">
     <h3>Matricular</h3>
-</article>
+    <ContractData drizzle={drizzle} drizzleState={drizzleState}
+              contract={"Asignatura"} method={"estaMatriculado"} methodArgs={[]}
+              render={bool => {
+                  if (bool == true) {
+                      return <p>"El alumno ya está matriculado"</p>
+                  }
+                  return <ContractForm drizzle={drizzle} drizzleState={drizzleState}
+                                       contract="Asignatura" method="automatricula"
+                                       labels={["Nombre Alumno", "Email Alumno"]}
+                  />
+              }}
+    />
+    </article>
 
 export default Matricular;
