731a2e5f4c45de0b7dea807a7d94581db3c0ceed
carlos <carlos.aznarol@gmail.com>
2021-01-12 19:35:48 +0100

    Version Final

--- dapp/src/components/Calificaciones/ObtenerLista.js
+++ dapp/src/components/Calificaciones/ObtenerLista.js
@@ -6,23 +6,24 @@ const {ContractData} = newContextComponents;
 const ObtenerLista = (props) => {
 
 
-    const [evaluacion, setEvaluacion] = useState(0);
+    let [listaEvaluaciones, setEvaluaciones] = useState(0);
 
-    const option = evaluacionIndex => (
+    const option = evaluacionesIndex => (
+
         <ContractData
             drizzle={props.drizzle}
             drizzleState={props.drizzleState}
             contract="Asignatura"
             method={"evaluaciones"}
-            methodArgs={[evaluacionIndex]}
-            render={ev => (
-                <option key={evaluacionIndex}> {ev.nombre}</option>
+            methodArgs={[evaluacionesIndex]}
+            render={evi => (
+                <option key={evaluacionesIndex}> {evi.nombre}</option>
             )}
         />
     );
 
     const dropdown = (
-        <select onChange={event => setEvaluacion(event.target.selectedIndex)}>
+        <select onChange={event => setEvaluaciones(event.target.selectedIndex)}>
             <ContractData
                 drizzle={props.drizzle}
                 drizzleState={props.drizzleState}
@@ -32,8 +33,10 @@ const ObtenerLista = (props) => {
             />
         </select>
     )
-
-    const row = (alumnoIndex, evaluacionIndex) => (
+    const titulo = evaluacionIndexTitulo =>(
+        <h2> Listado de notas de la evaluacion: E<sub>{evaluacionIndexTitulo}</sub> </h2>
+    )
+    const row = (alumnoIndex, evaluacionIndexRow) => (
         <tr>
             <th>A<sub>{alumnoIndex}</sub></th>
 
@@ -58,9 +61,9 @@ const ObtenerLista = (props) => {
                             drizzleState={props.drizzleState}
                             contract="Asignatura"
                             method="calificaciones"
-                            methodArgs={[alumnoAddr, evaluacionIndex]}
+                            methodArgs={[alumnoAddr, evaluacionIndexRow]}
                             render={nota => (
-                                <td key={"CalificacionesEvaluacion" + alumnoAddr}>
+                                <td key={"CalificacionesEvaluacion" + alumnoAddr + "-" + evaluacionIndexRow}>
                                     {nota.tipo === "0" && "N.P."}
                                     {nota.tipo === "1" && nota.calificacion / 10}
                                     {nota.tipo === "2" && (nota.calificacion / 10) + "(M.H.)"}
@@ -73,13 +76,14 @@ const ObtenerLista = (props) => {
         </tr>
     );
 
-    const table = evaluacionIndex => (
+
+    const table = evaluacionIndexTable => (
 
         <table>
             <thead>
             <th>#</th>
-            <th>Nombre</th>
-            <th>Nota</th>
+            <th>Nombre del Alumno</th>
+            <th>Calificacion del Alumno</th>
             </thead>
             <tbody>
             <ContractData
@@ -87,7 +91,7 @@ const ObtenerLista = (props) => {
                 drizzleState={props.drizzleState}
                 contract="Asignatura"
                 method="matriculasLength"
-                render={ml => Array.from({length: ml}, (_, alumnoIndex) => row(alumnoIndex, evaluacionIndex))}
+                render={ml => Array.from({length: ml}, (_, alumnoIndex) => row(alumnoIndex, evaluacionIndexTable))}
             />
             </tbody>
         </table>
@@ -95,9 +99,9 @@ const ObtenerLista = (props) => {
 
     return (
         <>
-            <h2> Listado de notas de la evaluacion </h2>
+            {titulo(listaEvaluaciones)}
             {dropdown}
-            {table(evaluacion)}
+            {table(listaEvaluaciones)}
         </>
     )
 };
